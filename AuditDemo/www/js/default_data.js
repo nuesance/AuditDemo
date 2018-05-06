72567ed31fa79c90f48c0c73a3a899cb30fe3fbf
Darshan Patel <darshan@pinatech.biz>
2018-05-06 18:58:48 +0530

    updated default data

--- AuditDemo/www/js/default_data.js
+++ AuditDemo/www/js/default_data.js
@@ -98,6 +98,206 @@
                 nonprofit_corp: true,
             },
         }
+    },
+    divvy: {
+        tp: {
+            legal_name: 'Divvy Inc',
+            virginia_id: '35680594383F002',
+            FEIN: '680594384',
+            compliance_code: 3521,
+            physical_address: {
+                street: 'PO Box 90258',
+                city: 'Lehi',
+                state: 'UT',
+                zip: '98008-9528',
+            },
+            mailing_address: {
+                street: 'PO Box 90258',
+                city: 'Lehi',
+                state: 'UT',
+                zip: '98008-9528',
+            },
+            mailing_sameas_physical: true,
+            initial_contact: {
+                name: 'Blake Murray',
+                phone: '425-943-8336',
+                phone_ext: null,
+                title: 'Tax Senior',
+                fax: '425-637-0253',
+                fax_ext: null,
+                email: 'johnbrezinski@coinstar.com'
+            },
+            representative: {
+                name: 'Blake Murray',
+                phone: '425-943-8336',
+                phone_ext: null,
+                title: 'Tax Senior',
+                fax: '425-637-0253',
+                fax_ext: null,
+                email: 'johnbrezinski@coinstar.com'
+            },
+        },
+
+        primary_auditor: {
+            auditor: 'Thomas Benton',
+            title: 'Interstate Auditor',
+            address: 'P O Box 2509',
+            city_st_zip: 'Dallas, GA 301 32-9998',
+            district: 'Interstate Audit Unit',
+            phone: '770-445-0867',
+            phone_ext: '12',
+            fax: '770-445-8069',
+            fex_ext: '34',
+            email: 'Thomas.Benton@tax.Virginia.gov',
+            user_id: 5907
+        },
+
+        details: {
+            legal_name: 'Divvy Inc',
+            filing_status: 'C',
+            date_began: '06/28/2011',
+            workpaper_id: 'MF-9C8',
+            status: 'Open',
+        },
+
+        supervisor: {
+            supervisor: 'Joanne Bialek',
+            title: 'Interstate Auditor Senior',
+            address: '123 Topfield Rd.',
+            city_st_zip: 'Ipswich, MA 01938',
+            email: 'Joanne.Bialek@tax.Virginia.gov',
+            phone: '978-356-3003',
+            phone_ext: '12',
+            fax: '978-356-2003',
+            fex_ext: '34',
+        },
+
+        audit_period: {
+            '2007': {
+                year_type: 'C',
+                year: '2007',
+                begin_date: null,
+                end_date: null,
+                return_filed: true,
+                extension_filed: true,
+                extended_thru: '10/15/2008',
+                final_return: false,
+                multistate_corp: true,
+                nonprofit_corp: false,
+            },
+            '2008': {
+                year_type: 'C',
+                year: '2008',
+                begin_date: null,
+                end_date: null,
+                return_filed: false,
+                extension_filed: false,
+                extended_thru: '10/15/2008',
+                final_return: false,
+                multistate_corp: false,
+                nonprofit_corp: true,
+            },
+        }
+    },
+    revolut: {
+        tp: {
+            legal_name: 'Revolut',
+            virginia_id: '35680594383F003',
+            FEIN: '680594385',
+            compliance_code: 3521,
+            physical_address: {
+                street: 'PO Box 90258',
+                city: 'Fairfax',
+                state: 'VA',
+                zip: '22030-9528',
+            },
+            mailing_address: {
+                street: 'PO Box 90258',
+                city: 'Fairfax',
+                state: 'VA',
+                zip: '22030-9528',
+            },
+            mailing_sameas_physical: true,
+            initial_contact: {
+                name: 'Vlad Yatsenko',
+                phone: '425-943-8336',
+                phone_ext: null,
+                title: 'Tax Senior',
+                fax: '425-637-0253',
+                fax_ext: null,
+                email: 'johnbrezinski@coinstar.com'
+            },
+            representative: {
+                name: 'Vlad Yatsenko',
+                phone: '425-943-8336',
+                phone_ext: null,
+                title: 'Tax Senior',
+                fax: '425-637-0253',
+                fax_ext: null,
+                email: 'johnbrezinski@coinstar.com'
+            },
+        },
+
+        primary_auditor: {
+            auditor: 'Thomas Benton',
+            title: 'Interstate Auditor',
+            address: 'P O Box 2509',
+            city_st_zip: 'Dallas, GA 301 32-9998',
+            district: 'Interstate Audit Unit',
+            phone: '770-445-0867',
+            phone_ext: '12',
+            fax: '770-445-8069',
+            fex_ext: '34',
+            email: 'Thomas.Benton@tax.Virginia.gov',
+            user_id: 5907
+        },
+
+        details: {
+            legal_name: 'Revolut',
+            filing_status: 'C',
+            date_began: '06/28/2011',
+            workpaper_id: 'MF-9C8',
+            status: 'Open',
+        },
+
+        supervisor: {
+            supervisor: 'Joanne Bialek',
+            title: 'Interstate Auditor Senior',
+            address: '123 Topfield Rd.',
+            city_st_zip: 'Ipswich, MA 01938',
+            email: 'Joanne.Bialek@tax.Virginia.gov',
+            phone: '978-356-3003',
+            phone_ext: '12',
+            fax: '978-356-2003',
+            fex_ext: '34',
+        },
+
+        audit_period: {
+            '2007': {
+                year_type: 'C',
+                year: '2007',
+                begin_date: null,
+                end_date: null,
+                return_filed: true,
+                extension_filed: true,
+                extended_thru: '10/15/2008',
+                final_return: false,
+                multistate_corp: true,
+                nonprofit_corp: false,
+            },
+            '2008': {
+                year_type: 'C',
+                year: '2008',
+                begin_date: null,
+                end_date: null,
+                return_filed: false,
+                extension_filed: false,
+                extended_thru: '10/15/2008',
+                final_return: false,
+                multistate_corp: false,
+                nonprofit_corp: true,
+            },
+        }
     }
 };
 
